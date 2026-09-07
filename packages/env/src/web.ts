a47a4bc96d0e326fcc40019e399bfb66b1f30954
Pedro Costa <pedrocostavgs@gmail.com>
2026-09-06 21:22:32 -0300

    build: initial config and biome format

--- packages/env/src/web.ts
+++ packages/env/src/web.ts
@@ -1,8 +1,12 @@
 import { createEnv } from "@t3-oss/env-core";
+import z from "zod";
 
 export const env = createEnv({
-  clientPrefix: "VITE_",
-  client: {},
-  runtimeEnv: (import.meta as any).env,
-  emptyStringAsUndefined: true,
+	clientPrefix: "VITE_",
+	client: {
+		VITE_SERVER_URL: z.url(),
+	},
+	// biome-ignore lint/suspicious/noExplicitAny: this will be validated at runtime
+	runtimeEnv: (import.meta as any).env,
+	emptyStringAsUndefined: true,
 });
